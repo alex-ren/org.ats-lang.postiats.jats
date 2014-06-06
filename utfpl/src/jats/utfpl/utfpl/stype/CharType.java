9565e262d0120438d6ac365d594bfff351f68800
Alex (Bluelight) <alex.ren2006@gmail.com>
2014-06-06 01:08:42 -0400

    update

---
+++ utfpl/src/jats/utfpl/utfpl/stype/CharType.java
@@ -0,0 +1,9 @@
+package jats.utfpl.utfpl.stype;
+
+public class CharType extends EleType {
+    public static CharType cInstance = new CharType();
+    private CharType() {
+        
+    }
+
+}
---
+++ utfpl/src/jats/utfpl/utfpl/stype/TupleType.java
@@ -0,0 +1,25 @@
+package jats.utfpl.utfpl.stype;
+
+import java.util.List;
+
+public class TupleType extends BoxedType {
+	private List<SortType> m_types;
+	
+	public TupleType(List<SortType> types) {
+		m_types = types;
+	}
+
+	@Override
+    public boolean equals(ISType ty) {
+		if (this == ty) {
+			return true;
+		} else if (ty instanceof TupleType) {
+			TupleType right = (TupleType)ty;
+			return m_types.equals(right.m_types);
+		} else {
+			return false;
+		}
+
+    }
+
+}
